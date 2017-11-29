c6d6a1042174cd54e6ff24755cf0ef4d93e2cdb7
Oleksandr Husak <valavanca@gmail.com>
2017-11-29 16:15:06 +0100

    apply AdaBoost and DecisionTree

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,14 +2,14 @@
 
 import sys
 import pickle
+import numpy as np
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
 ## USER
-from sklearn.feature_selection import f_regression
-
+from time import time
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -30,6 +30,8 @@ with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop("TOTAL",0)
+#data_dict.pop("THE TRAVEL AGENCY IN THE PARK",0)
 
 
 ### Task 3: Create new feature(s)
@@ -59,46 +61,39 @@ features_list += ['percent_shared_receipt', 'percent_from_poi_to_this_person'] #
 #	print name, " :: ", my_dataset[name]['shared_receipt_with_poi']
 #	print "______________________________________________________________"
 
-
+##############################################################################
+######################           DATA            #############################
+ 
 ### Extract features and labels from dataset for local testing
+
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=72)
 
 from sklearn.feature_selection import SelectKBest
 skb = SelectKBest()
 skb.fit(features,labels)
 
+skb_short = SelectKBest()
+skb_short.fit(features_train,labels_train)
+
 print "\n ********** Select K-Bests **********"
 for i, s in enumerate((skb.scores_)):
     print " [", s, "] ", features_list[i+1]
 
+print "\n __________________________________short"
+for i, s in enumerate((skb_short.scores_)):
+    print " [", s, "] ", features_list[i+1]
+
 
 ### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
-from sklearn.ensemble import AdaBoostClassifier
-abc = AdaBoostClassifier()
-
-from sklearn.svm import SVC
-svc = SVC()
-
-from sklearn.neighbors import NearestCentroid
-nc = NearestCentroid()
-
-from sklearn.ensemble import RandomForestClassifier
-rfc = RandomForestClassifier()
-
 
 #####  Pipe
 print "\n ********** Pipe **********"
+from sklearn.pipeline import Pipeline
 
 from sklearn import preprocessing
 min_max_scaler = preprocessing.MinMaxScaler()
@@ -106,47 +101,96 @@ min_max_scaler = preprocessing.MinMaxScaler()
 from sklearn.decomposition import PCA
 pca = PCA()
 
-from sklearn.pipeline import Pipeline
-estimators = [('scaler', min_max_scaler),('SKB', skb),('reduce_dim', pca), ('clf', nc)]
-pipeline = Pipeline(estimators)
+from sklearn.neighbors import NearestCentroid
+nc = NearestCentroid()
 
-
-
+#estimators = [('scaler', min_max_scaler),('SKB', skb),('reduce_dim', pca), ('clf', nc)]
+#pipeline = Pipeline(estimators)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 print "\n ********** Tune **********"
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.cross_validation import StratifiedShuffleSplit
 
-cv = StratifiedShuffleSplit(labels, n_iter=10, random_state = 42)
-param_spaces = {
-    'SKB__k':[3,4,5,6],
-    'reduce_dim__n_components':[2,3],
-	# tune    NearestCentroid
-    'clf__metric': ['euclidean','manhattan']
-}
+cv = StratifiedShuffleSplit(labels_train, n_iter=8, random_state = 42)
 
-gs = GridSearchCV(pipeline,param_grid = param_spaces, n_jobs = -1,cv = cv, scoring = 'recall',verbose=10)
-gs.fit(features, labels)
+##############################################################################
+######################                          ##############################
+######################    AdaBoostClassifier    ##############################
+t0 = time()
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier()
 
-print "\n ********** Best **********"
-clf = gs.best_estimator_
-print "Best params: ", gs.best_params_
-# print "Best estimator: ", gs.best_estimator_
+clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', clf)])
+params = dict(feature_selector__k=[4,5,6,8],
+	      reduce_dim__n_components = [1,2,3,4],
+              clf__n_estimators=[100,125,150,175,200,1000],
+	      clf__algorithm=['SAMME', 'SAMME.R'])
+skl_clf = GridSearchCV(clf_pipe, param_grid=params, cv = cv, scoring = 'recall', verbose=1)
+skl_clf.fit(features_train,labels_train)
+#skl_clf.best_estimator_
+print "Best params Adaboost: ", gs.best_params_
+print "Searching time: ", round(time()-t0, 3), "s"
+
+labels_pred = skl_clf.predict(features_test)
+print("Score: ", skl_clf.best_score_)
+print("Accuracy tree: ", accuracy_score(labels_test, labels_pred))
 
 
+##############################################################################
+######################                           #############################
+######################            SVC            #############################
+'''
+t0 = time()
+from sklearn import svm
+#from sklearn.svm import SVC
+clf = svm.SVC(kernel="linear")
+clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', clf)])
+params = dict(feature_selector__k=[4],
+	      reduce_dim__n_components = [2],
+              clf__C=[1],
+	      #clf__kernel=['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'], 
+	      #clf__gamma=[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]
+	)
+skl_clf = GridSearchCV(clf_pipe, param_grid=params, scoring = 'recall', verbose=3)
+skl_clf.fit(features_train,labels_train)
+#skl_clf.best_estimator_
+print "Best params SVC: ", gs.best_params_
+print "Searching time: ", round(time()-t0, 3), "s"
 
+labels_pred = grid.predict(features_test)
+print("Score: ", grid.best_score_)
+print("Accuracy SVC: ", accuracy_score(labels_test, labels_pred))
+'''
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+##############################################################################
+######################                            ############################
+######################            Tree            ############################
+
+'''
+t0 = time()
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', clf)])
+params = dict(feature_selector__k=[5,6,7,8,9],
+	      reduce_dim__n_components = [2,3,4,5],
+              clf__max_features = ['auto', 2],
+	      #clf__min_samples_split = [2,4,6,10]
+	)
+skl_clf = GridSearchCV(clf_pipe, param_grid=params,cv=cv, scoring = 'recall', verbose=1)
+skl_clf.fit(features_train,labels_train)
+#skl_clf.best_estimator_
+print "Best params tree: ", skl_clf.best_params_
+print "Searching time: ", round(time()-t0, 3), "s"
+
+labels_pred = skl_clf.predict(features_test)
+print("Score: ", skl_clf.best_score_)
+print("Accuracy tree: ", accuracy_score(labels_test, labels_pred))
+'''
+
+##############################################################################
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
@@ -154,7 +198,7 @@ features_train, features_test, labels_train, labels_test = \
 ### generates the necessary .pkl files for validating your results.
 print "__________________________________________________"
 
-#dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
 
 import tester
 tester.main()
