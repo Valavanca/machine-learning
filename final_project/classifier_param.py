76efcd83d9cf073956c100cec054f931ea9e33a1
Oleksandr Husak <valavanca@gmail.com>
2017-11-30 23:45:08 +0100

    classifier in the loop

--- final_project/classifier_param.py
+++ final_project/classifier_param.py
@@ -2,22 +2,43 @@
 config = {
 	'Adaboost': {
 		 #'pipeline': [('feature_selector', skb),('reduce_dim', pca), ('clf', clf)],
-		 'params': dict(feature_selector__k=[4,5,6,8],
+		 'params': dict(feature_selector__k=[4,5,7,8,9,12],
 			      reduce_dim__n_components = [1,2,3,4],
-			      clf__n_estimators=[100,125,150,175,200,1000],
+			      clf__n_estimators=[100,125,150,175,200,1000,5000],
 			      clf__algorithm=['SAMME', 'SAMME.R'])
 		},
 	'RandomTree': {
-		 'params': dict(feature_selector__k=[4,5,6,7,8,9],
+		 'params': dict(feature_selector__k=[4,5,6,8,9,11,12],
 			      reduce_dim__n_components = [2,3,4],
 			      clf__criterion = ['gini','entropy'],
 			      clf__min_samples_split = [2,4,6,10])
 		},
 	'DecisionTree': {
-		 'params': dict(feature_selector__k=[5,6,7,8,9],
+		 'params': dict(feature_selector__k=[5,6,7,8,9,11,12],
 			      reduce_dim__n_components = [2,3,4,5],
 			      clf__max_features = ['auto', 2],
+			      clf__max_depth = [2,4,5,6,8,10],
+			      clf__splitter = ['best', 'random']
+		            )
+		},
+	'GaussianNB': {
+		 'params': dict(feature_selector__k=[6,7,8,9,11,12],
+			      reduce_dim__n_components = [2,3,4,5,6],
+		            )
+		},
+	'MultinomialNB': {
+		 'params': dict(feature_selector__k=[5,6,7,8,9,11,12],
+			      reduce_dim__n_components = [2,3,4,5],
+			      clf__alpha = [10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001],
 			      #clf__min_samples_split = [2,4,6,10]
+		            )
+		},
+	'SVC': {
+		 'params': dict(feature_selector__k=[5,6],
+			      reduce_dim__n_components = [2,3],
+			      clf__C=[1],
+			      clf__kernel=['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'], 
+			      #clf__gamma=[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]
 		            )
 		},
 }
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,6 +10,7 @@ from tester import dump_classifier_and_data
 
 ## USER
 from time import time
+import tester
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -32,10 +33,11 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 2: Remove outliers
 data_dict.pop("TOTAL",0)
 data_dict.pop("THE TRAVEL AGENCY IN THE PARK",0)
-for element in data_dict:
-    for feature in features_list[1:]:
-        if feature == 'fraction_from_this_person_to_poi' or feature == 'fraction_from_poi_to_this_person': continue
-        if data_dict[element][feature] < 0: data_dict[element][feature] = "NaN"
+# remove negative values
+for element in data_dict: 
+   for feature in features_list[1:]:
+       if feature == 'fraction_from_this_person_to_poi' or feature == 'fraction_from_poi_to_this_person': continue
+       if data_dict[element][feature] < 0: data_dict[element][feature] = "NaN"
 
 
 ### Task 3: Create new feature(s)
@@ -74,7 +76,7 @@ data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
 from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=72)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.15, random_state=62)
 
 ### Task 4: Try a varity of classifiers 
 ### Pipe
@@ -104,7 +106,6 @@ for i, s in enumerate((skb.scores_)):
 ###########################     Tune     ######################################
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-
 print "\n --- Tune ---"
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
@@ -114,143 +115,50 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
 
 from classifier_param import config
-cv = StratifiedShuffleSplit(labels_train, n_iter=8, random_state = 23)
+cv = StratifiedShuffleSplit(labels_train, n_iter=8, random_state = 33)
 
-####### selection parameters for the algorithm (pipe->grid)
+# selection parameters for the algorithm (pipe->grid)
 def tune_classifier( algoritm ):
 	#t0 = time()
-	pipe = Pipeline([('feature_selector', algoritm['skb']),('reduce_dim', algoritm['pca']), ('clf', algoritm['clf'])])
+	pipe = Pipeline([('min/max scaler', preprocessing.MinMaxScaler(feature_range=(0.0, 1.0))),
+			('feature_selector', algoritm['skb']),
+			('reduce_dim', algoritm['pca']), 
+			('clf', algoritm['clf'])])
 	skl_clf = GridSearchCV(pipe, param_grid=algoritm['params'], cv=cv, scoring='recall', verbose=1)
 	#print "\n Searching time: ", round(time()-t0, 3), "s"
 	return skl_clf;
 
-
-
-######################    AdaBoostClassifier    ##############################
-
-'''
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
-clf = AdaBoostClassifier(DecisionTreeClassifier(criterion='entropy'))
-
-clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', clf)])
-params = dict(feature_selector__k=[4,5,6,8],
-	      reduce_dim__n_components = [1,2,3,4],
-              clf__n_estimators=[100,125,175,200,1000],
-	      clf__algorithm=['SAMME', 'SAMME.R'])
-skl_clf = GridSearchCV(clf_pipe, param_grid=params, cv = cv, scoring = 'recall', verbose=1)
-skl_clf.fit(features_train,labels_train)
-#skl_clf.best_estimator_
-print "Best params Adaboost: ", skl_clf.best_params_
-'''
-###############################################################################
-'''
-param_grid = {'n_estimators': [10,55,60,100],\
-'learning_rate':[1.,2.,5.],\
-'algorithm': ['SAMME', 'SAMME.R']}
-
-from sklearn.ensemble import AdaBoostClassifier
-
-skl_clf = GridSearchCV(AdaBoostClassifier(),param_grid,scoring="recall")
-skl_clf.fit(features,labels)
-clf = skl_clf.best_estimator_
-print "Searching time: ", round(time()-t0, 3), "s"
-
-labels_pred = skl_clf.predict(features_test)
-print("Score: ", skl_clf.best_score_)
-print("Accuracy tree: ", accuracy_score(labels_test, labels_pred))
-'''
-
-
-######################            SVC            #############################
-'''
-t0 = time()
-from sklearn import svm
-#from sklearn.svm import SVC
-clf = svm.SVC(kernel="linear")
-clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', clf)])
-params = dict(feature_selector__k=[4],
-	      reduce_dim__n_components = [2],
-              clf__C=[1],
-	      #clf__kernel=['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'], 
-	      #clf__gamma=[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]
-	)
-skl_clf = GridSearchCV(clf_pipe, param_grid=params, scoring = 'recall', verbose=3)
-skl_clf.fit(features_train,labels_train)
-#skl_clf.best_estimator_
-print "Best params SVC: ", gs.best_params_
-print "Searching time: ", round(time()-t0, 3), "s"
-
-labels_pred = grid.predict(features_test)
-print("Score: ", grid.best_score_)
-print("Accuracy SVC: ", accuracy_score(labels_test, labels_pred))
-'''
-
-######################            Tree            ############################
-
-'''
-t0 = time()
-from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier(criterion='entropy')
-clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', clf)])
-params = dict(feature_selector__k=[5,6,7,8,9],
-	      reduce_dim__n_components = [2,3,4,5],
-              clf__max_features = ['auto', 2],
-	      #clf__min_samples_split = [2,4,6,10]
-	)
-skl_clf = GridSearchCV(clf_pipe, param_grid=params,cv=cv, scoring = 'recall', verbose=1)
-skl_clf.fit(features_train,labels_train)
-#skl_clf.best_estimator_
-print "Best params tree: ", skl_clf.best_params_
-print "Searching time: ", round(time()-t0, 3), "s"
-
-labels_pred = skl_clf.predict(features_test)
-print("Score: ", skl_clf.best_score_)
-print("Accuracy tree: ", accuracy_score(labels_test, labels_pred))
-'''
-
-######################         Random Tree        ############################
-'''
-t0 = time()
-from sklearn.ensemble import RandomForestClassifier
-clf_pipe = Pipeline([('feature_selector', skb),('reduce_dim', pca), ('clf', RandomForestClassifier())])
-params = dict(feature_selector__k=[4,5,6,7,8,9],
-	      reduce_dim__n_components = [2,3,4],
-              clf__criterion = ['gini','entropy'],
-	      clf__min_samples_split = [2,4,6,10]
-	)
-skl_clf = GridSearchCV(clf_pipe, param_grid=params, cv=cv, scoring ='recall', verbose=1)
-skl_clf.fit(features_train,labels_train)
-clf = skl_clf.best_estimator_
-print "Best params random tree: ", skl_clf.best_params_
-print "Searching time: ", round(time()-t0, 3), "s"
-
-labels_pred = skl_clf.predict(features_test)
-print("Score: ", skl_clf.best_score_)
-print("Accuracy tree: ", accuracy_score(labels_test, labels_pred))
-
-
-'''
-
+# algoritms for loop
 algoritms = {
-#	'Adaboost': {'skb': skb, 'pca': pca, 'clf': AdaBoostClassifier(DecisionTreeClassifier(criterion='entropy')),
-#			'params': config['Adaboost']['params']
-#			},
+	#'Adaboost': {'skb': skb, 'pca': pca, 'clf': AdaBoostClassifier(DecisionTreeClassifier(criterion='entropy')),
+	#		'params': config['Adaboost']['params']
+	#		},
 	'RandomTree': {'skb': skb, 'pca': pca, 'clf': RandomForestClassifier(),
 			'params': config['RandomTree']['params']
 			},
 	'DecisionTree': {'skb': skb, 'pca': pca, 'clf': DecisionTreeClassifier(criterion='entropy'),
 			'params': config['DecisionTree']['params']
 			},
+	'GaussianNB': {'skb': skb, 'pca': pca, 'clf': GaussianNB(),
+			'params': config['GaussianNB']['params']
+			},
+	#'MultinomialNB': {'skb': skb, 'pca': pca, 'clf': MultinomialNB(),
+	#		'params': config['MultinomialNB']['params']
+	#		},
+	#'SVC': {'skb': skb, 'pca': pca, 'clf': svm.SVC(),
+	#		'params': config['SVC']['params']
+	#		},
 }
 
-# choose algoritm from classifier list
+# select algoritm from classifier list
 temp_score = 0
 temp_accuracy = 0
 temp_algoritm = ""
-clf = GaussianNB()
+clf = MultinomialNB() #default
 
 for item in algoritms:
 	t0 = time()
@@ -262,8 +170,12 @@ for item in algoritms:
 	labels_pred = temp_result.predict(features_test)
 	accuracy = accuracy_score(labels_test, labels_pred)
 	print " Score: ", temp_result.best_score_
-	print " Accuracy : ", accuracy
-
+	print " Accuracy: ", accuracy
+	print " Best params: ", temp_result.best_params_
+	print " _____________"
+	clf = temp_result.best_estimator_
+	tester.main()
+	
 	if temp_result.best_score_ > temp_score:
 		temp_score = temp_result.best_score_
 		temp_accuracy = accuracy
@@ -278,7 +190,6 @@ for item in algoritms:
 print "\n", "*"*70, "\n* Chosen ", temp_algoritm, ("score: ", temp_score, "accuracy", temp_accuracy),"\n", "*"*70
 
 ##############################################################################
-
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
@@ -286,7 +197,5 @@ print "\n", "*"*70, "\n* Chosen ", temp_algoritm, ("score: ", temp_score, "accur
 print "\n --- Test ---\n", "_"*70
 
 dump_classifier_and_data(clf, my_dataset, features_list)
-
-import tester
 tester.main()
 
